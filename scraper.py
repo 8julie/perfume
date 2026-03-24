d48d639f37d4ccd4711a89b007cb13ae0bdc8f30
Julie Bui <minhab@uci.edu>
2026-03-23 22:55:46 -0700

    fixed scraper

--- scraper.py
+++ scraper.py
@@ -1,26 +1,21 @@
 import time # being nice <3
 import requests
+import re
 from bs4 import BeautifulSoup
 
 def scrape(site_url):
     """A simple scraper"""
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
+
+    profile_links = soup.find_all('a')
+
+    for link in profile_links:
+        print(link)
+    
     time.sleep(4)
 
-    return soup
-
-def parse(soup:BeautifulSoup):
-    fragbase = soup.select('td > *')
-    fragbase = soup.select()
-
-
-    for item in parsed:
-        print (item)
-
-# Use(s): fragrance agents</td>, <td><a href="#" onclick="openMainWindow('/data/pb1114361.html');return false;">amber woody specialty</a>
 
 if __name__ == '__main__':
     url = 'https://www.thegoodscentscompany.com/peb-az.html'
-    soup = scrape(url)
-    parse(soup)
+    scrape(url)
