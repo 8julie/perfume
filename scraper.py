cee0fe43aceb5a9a4621ee91825e5964c2023d1f
Julie Bui <minhab@uci.edu>
2026-03-24 00:14:42 -0700

    refactored

--- scraper.py
+++ scraper.py
@@ -3,36 +3,9 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-import json
-
 from urllib.parse import urlparse, urljoin
 
-def scrape(site_url):
-    """A simple scraper"""
-
-    res, base_no = [], 1
-    base_url = urljoin(urlparse(site_url).scheme, urlparse(site_url).netloc)
-
-    response = requests.get(url)
-    time.sleep(4) # optional ..... i'm just being nice
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Selects
-    pattern = r'data.*html'
-    on_clicks = soup.find_all('a', {'onclick' : re.compile(pattern)})
-
-    # Parses and puts it somewhere
-    for item in on_clicks:
-        name = item.get_text().replace("specialty", "")
-        link = re.findall(pattern, item['onclick'])[0]
-        absolute_link = base_url + "/" + link
-
-        data = {'name': name, 'link': absolute_link}
-
-        res.append(data)
-        base_no += 1
-
-    return res
+import json
 
 def save(file_name, info):
     """Writes data into a json file"""
@@ -40,7 +13,50 @@ def save(file_name, info):
     with open(file_name, 'w', encoding='latin-1') as f:
         json.dump(info, f, indent=8, ensure_ascii=False)
 
-if __name__ == '__main__':
+
+class Scraper():
+    def __init__(self, url):
+        self.url = url # intended to be the index
+        self.parsed_url = urlparse(self.url)
+        self.base_url = urljoin(self.parsed_url.scheme, self.parsed_url.netloc)
+
+        self.scrape()
+        self.saveIndex()
+
+
+    def scrape(self):
+        """A simple scraper"""
+        response = requests.get(url)
+        self.soup = BeautifulSoup(response.text, 'html.parser')
+        time.sleep(4) # optional ..... i'm just being nice
+
+    
+    def saveIndex(self):
+        """Assumes self.url contains index, gets items/frag bases"""
+
+        res = []
+
+        # Selects
+        link_pattern = r'data.*html'
+        on_clicks = self.soup.find_all('a', {'onclick' : re.compile(link_pattern)})
+
+        # Parses
+        for item in on_clicks:
+            name = item.get_text().replace("specialty", "")
+            link = re.findall(link_pattern, item['onclick'])[0]
+            absolute_link = self.base_url + "/" + link
+
+            data = {'name': name, 'link': absolute_link}
+
+            res.append(data)
+
+        # Puts it somewhere
+        
+        fn = "index"
+        save(fn, res)
+
+    
+
+if __name__ == "__main__":
     url = 'https://www.thegoodscentscompany.com/peb-az.html' # very rudimentary but it's just 1 link! who cares
-    res = scrape(url)
-    save("peb", res)
+    Scraper(url)
